ead89d0b4a3363916c03aec899d3a163ebef8217
mulerinho <alan_mulero@hotmail.com>
2024-01-10 16:35:40 -0300

    Criando função para entrada de dados e lista.

--- palavra.py
+++ palavra.py
@@ -1,18 +1,29 @@
-palavra_secreta = 'PATO'
+
+palavra_secreta = 'banana'
 acertou = False
 errou = False
+# criando lista usando o for comprehension.
+lista = ['_' for item in range(0, len(palavra_secreta))]
 
-while not acertou and not errou:
+def entrada():
+    # Capturando dados do usuario.
+    letra = input('Digite uma letra: ').lower()
+    while len(letra) > 1:
+        print('Digite apenas uma letra por vez.')
+        break
+    return letra
 
 
-    def captura_palavra():
-        letra = input('Digite uma letra: ').upper()
-        while len(letra) > 1:
-            print('Digite apenas uma letra.')
-            break
-        return letra
 
-    resposta = captura_palavra()
-    print(resposta)
+while (not acertou and not errou):
 
     
+    
+    letra = entrada() # Chamando função
+    # percorrendo lista e guardando as letra acertadas.
+    index = 0
+    for i in palavra_secreta:
+        if(letra == i):
+            lista[index] = letra
+        index += 1    
+    print(lista)    
